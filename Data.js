758f2173d7dda9aefe388493be0bd3c1265b3f0e
Venuveda <venu.veda@syf.com>
2021-04-06 09:25:29 +0000

    Test

--- Data.js
+++ Data.js
@@ -2,3 +2,14 @@
 for(let c=22;c<=26;c++){
     console.log(10);
 }
+//print numbers from 1 to 10
+for(let c=1;c<=10;c++){ //c++ = c=c+1
+    console.log(c);
+}
+
+//print odd numbers between 10 and 20
+for(let c=11;c<=20;c+=2){ //c+=2 = c=c+2
+    console.log(c);
+}
+
+TextDecoderStream
