32bddf418a1d2306b1c7c93e3708b616c4bb1476
Venuveda <venu.veda@syf.com>
2021-04-08 11:05:40 +0000

    objectconstructor.js

---
+++ objectconstructor.js
@@ -0,0 +1,30 @@
+function CreateTool(name, version, languages, date, time) {
+    this.name = name;
+    this.version = version;
+    this.languages = languages;
+    this.date = date;
+    this. time = time;
+
+    this.start=function(){
+        console.log(this.name+" is started");        
+    }
+    this.stop=function(){
+        console.log(this.name+" is stopped");   
+    }
+}
+
+let tool = new CreateTool("selenium",4,["java","js","python","ruby","c#"]);
+let tool2 = new CreateTool("playwright",1,["java","js","python","c#"]);
+
+console.log(tool["name"]);
+console.log(tool["version"]);
+
+console.log(tool.name);
+console.log(tool.version);
+console.log(tool.languages);
+
+tool.start()
+tool2.start()
+
+tool.stop()
+tool2.stop()
